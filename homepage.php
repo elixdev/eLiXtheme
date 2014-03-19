6cec039270ae72090534f8495f29fc2a58b954ca
eLiX <github@elix.info>
2014-03-19 17:15:01 +0100

    Přidána šablona pro úvodní stránku

---
+++ homepage.php
@@ -0,0 +1,11 @@
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }
+
+require dirname(__FILE__).'/header.inc.php';
+?>
+<div class="row">
+	<div class="col-md-12">
+		<?php get_page_content(); ?>
+	</div>
+</div>
+
+<?php require dirname(__FILE__).'/footer.inc.php'; ?>
